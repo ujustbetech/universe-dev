f6255a401e54917ba109b39b10290d933ce8530e
Ruchita Godse <ruchitagodse217@gmail.com>
2025-12-29 12:46:55 +0530

    nav ui

--- component/Navbar.js
+++ component/Navbar.js
@@ -1,41 +1,36 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from "next/router";
-
-import { AiOutlineHome } from "react-icons/ai";
 import { MdEventAvailable, MdOutlineContentPaste, MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { RiListSettingsLine } from "react-icons/ri";
 import { FaRegUser, FaPeopleArrows, FaRegEye } from "react-icons/fa";
-import { BsCake2, BsCashCoin } from "react-icons/bs";
+import { BsCashCoin } from "react-icons/bs";
 import { GrGroup } from "react-icons/gr";
 import { GiCosmicEgg } from "react-icons/gi";
 
 const Navbar = (props) => {
-  const router = useRouter();
-
   return (
     <>
       {props.loading ? (
-        <div className="loader">
-          <span className="loader2"></span>
-        </div>
+        <div className="loader"><span className="loader2"></span></div>
       ) : (
         <nav className={props.expand ? 'm-navbar expand' : 'm-navbar unexpand'}>
           <ul>
-   <li>
-                            <Link href="/admin/event/userlist">
-                                <span className="icons"><FaRegUser /></span>
-                                <span className="linklabel">Orbiters</span>
-                                <span className="submenuIcon"><MdOutlineKeyboardArrowDown /></span>
-                            </Link>
-                            <ul>
-                                <li><Link href="/admin/event/userlist">Orbiters Profiling</Link></li>
-                            </ul>
-                        </li>
+
+            {/* Orbiters */}
+            <li>
+              <span className="nav-link" tabIndex={0}>
+                <span className="icons"><FaRegUser /></span>
+                <span className="linklabel">Orbiters</span>
+                <MdOutlineKeyboardArrowDown className="submenuIcon" />
+              </span>
+              <ul>
+                <li><Link href="/admin/event/userlist">Orbiters Profiling</Link></li>
+              </ul>
+            </li>
 
             {/* Monthly Meet */}
             <li>
-              <span className="nav-link">
+              <span className="nav-link" tabIndex={0}>
                 <span className="icons"><MdEventAvailable /></span>
                 <span className="linklabel">Monthly Meet</span>
                 <MdOutlineKeyboardArrowDown className="submenuIcon" />
@@ -46,9 +41,9 @@ const Navbar = (props) => {
               </ul>
             </li>
 
-      {/* Referrals */}
+            {/* Referrals */}
             <li>
-              <span className="nav-link">
+              <span className="nav-link" tabIndex={0}>
                 <span className="icons"><FaPeopleArrows /></span>
                 <span className="linklabel">Referrals</span>
                 <MdOutlineKeyboardArrowDown className="submenuIcon" />
@@ -59,10 +54,9 @@ const Navbar = (props) => {
               </ul>
             </li>
 
-        
             {/* Conclave */}
             <li>
-              <span className="nav-link">
+              <span className="nav-link" tabIndex={0}>
                 <span className="icons"><GrGroup /></span>
                 <span className="linklabel">Conclave</span>
                 <MdOutlineKeyboardArrowDown className="submenuIcon" />
@@ -73,10 +67,9 @@ const Navbar = (props) => {
               </ul>
             </li>
 
-      
             {/* Prospects */}
             <li>
-              <span className="nav-link">
+              <span className="nav-link" tabIndex={0}>
                 <span className="icons"><GiCosmicEgg /></span>
                 <span className="linklabel">Prospects</span>
                 <MdOutlineKeyboardArrowDown className="submenuIcon" />
@@ -86,26 +79,23 @@ const Navbar = (props) => {
                 <li><Link href="/prospectadmin/event/manageProspect">Manage Prospects</Link></li>
               </ul>
             </li>
-{/* Event */}
-<li>
-  <span className="nav-link">
-    <span className="icons"><MdEventAvailable /></span>
-    <span className="linklabel">Event</span>
-    <MdOutlineKeyboardArrowDown className="submenuIcon" />
-  </span>
 
-  <ul>
-    <li>
-      <Link href="/event/addevent">Add Event</Link>
-    </li>
-    <li>
-      <Link href="/event/eventcategory">Event Listing</Link>
-    </li>
-  </ul>
-</li>
-              {/* Content */}
+            {/* Event */}
             <li>
-              <span className="nav-link">
+              <span className="nav-link" tabIndex={0}>
+                <span className="icons"><MdEventAvailable /></span>
+                <span className="linklabel">Event</span>
+                <MdOutlineKeyboardArrowDown className="submenuIcon" />
+              </span>
+              <ul>
+                <li><Link href="/event/addevent">Add Event</Link></li>
+                <li><Link href="/event/eventcategory">Event Listing</Link></li>
+              </ul>
+            </li>
+
+            {/* Content */}
+            <li>
+              <span className="nav-link" tabIndex={0}>
                 <span className="icons"><MdOutlineContentPaste /></span>
                 <span className="linklabel">Content</span>
                 <MdOutlineKeyboardArrowDown className="submenuIcon" />
@@ -118,7 +108,7 @@ const Navbar = (props) => {
 
             {/* Users */}
             <li>
-              <span className="nav-link">
+              <span className="nav-link_jobs" tabIndex={0}>
                 <span className="icons"><FaRegUser /></span>
                 <span className="linklabel">Users</span>
                 <MdOutlineKeyboardArrowDown className="submenuIcon" />
@@ -130,9 +120,9 @@ const Navbar = (props) => {
               </ul>
             </li>
 
-         {/* CP */}
+            {/* CP */}
             <li>
-              <span className="nav-link">
+              <span className="nav-link" tabIndex={0}>
                 <span className="icons"><BsCashCoin /></span>
                 <span className="linklabel">CP</span>
                 <MdOutlineKeyboardArrowDown className="submenuIcon" />
@@ -143,7 +133,7 @@ const Navbar = (props) => {
               </ul>
             </li>
 
-            {/* User Logins */}
+            {/* Direct Links */}
             <li>
               <Link href="/admin/PageVisit" className="nav-link">
                 <span className="icons"><FaRegEye /></span>
@@ -151,7 +141,6 @@ const Navbar = (props) => {
               </Link>
             </li>
 
-            {/* Upload Excel */}
             <li>
               <Link href="/admin/event/upload" className="nav-link">
                 <span className="icons"><RiListSettingsLine /></span>
